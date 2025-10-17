4200b5db52004179a5968da3bac2cd57f920d269
Harshavardhan Bajoria <62978274+HVbajoria@users.noreply.github.com>
2025-10-17 11:00:16 +0530

    Add .env File Support for Secure Configuration Management (#40)
    * Add .env File Support for Secure Configuration Management
    
    * Updated calling OPENAI_API_KEY and Readme.md
    
    ---------
    
    Co-authored-by: Aaditya Gupta <aadityagupta400@gmail.com>

--- src/repello_agent_wiz/analyzers/threat_modelling.py
+++ src/repello_agent_wiz/analyzers/threat_modelling.py
@@ -1,10 +1,10 @@
-import os
 import json
 
 from openai import OpenAI
 import importlib.resources as pkg_resources
 
 from repello_agent_wiz import analyzers
+from repello_agent_wiz.config import OPENAI_API_KEY
 
 
 def generate_maestro_analysis_report(json_path: str):
@@ -26,7 +26,7 @@ def generate_maestro_analysis_report(json_path: str):
     sys_prompt = sys_prompt.replace("<JSON>", graph_json)
 
     # Initialize the OpenAI client properly
-    client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+    client = OpenAI(api_key=OPENAI_API_KEY)
 
     # Use the client instance to create the completion
     response = client.chat.completions.create(
